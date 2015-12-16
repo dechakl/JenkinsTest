14679e55645d0b4b47f72f2d086cb1fc313a15e8
dechakl <decha@klnetwork.com>
2015-12-16 09:34:18 +0700

    update assert

---
+++ JenkinsTest/JenkinsTest/NunitTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace JenkinsTest
+{
+    public class NunitTest
+    {
+        [Test]
+        public void TestAssert()
+        {
+            var a = 5;
+            Assert.AreEqual(5, a);
+        }
+    }
+}
